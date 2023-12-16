a4bc8dd18fe81b063c3c5a10484d20c51f054def
unknown <weabuddin@gmail.com>
2023-12-16 14:41:14 +0700

    coba lagi 12

--- config/database.js
+++ config/database.js
@@ -1,5 +1,8 @@
 import { Sequelize } from 'sequelize';
 import mysql2 from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
